704133474efc75fea96703908fed24b0b4d15188
xshqhua <xshqhua@gmail.com>
2016-10-20 22:23:47 +0800

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,35 @@ public class Main{
         new Main().init();
     }
 	public void init() {
-		
-		int m = 0;
-		fun(m);
-		
+		/*Scanner scanner = new Scanner(System.in);
+		while(scanner.hasNext()){
+			int m = scanner.nextInt();
+			fun(m);
+		}
+		scanner.close();*/
+		int [] arr = {1,2,49,34,73,44,3,4,5,6,7};
+		fun(arr);
 	}
-    public int fun(float data){
-        int m = (int) Math.floor(data);
-        float tep = data - m;
-        if(tep>=0.5){
-            m++;
-        }
-        return m;
+    public int fun(int[] data){
+    	int m=0;
+    	int max = data[0];
+    	int sec = data[0];
+    	
+    	for(int i=0;i<data.length;i++){
+    		if(data[i]>sec){
+    			//1
+    			sec = data[i];
+    		}
+    		if(sec>max){
+    			max += sec;
+    			//2
+    			sec = max-sec;
+    			max-=sec;
+    		}
+    	}
+    	//3
+    	System.out.println(max);
+    	System.out.println(sec);
+    	return max;
     }
 }
