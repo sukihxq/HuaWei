31d75d8b61b033e87b7c9286d5c49e7b79629a8a
xshqhua <xshqhua@gmail.com>
2016-10-21 12:05:48 +0800

    2016-10-21

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,18 @@
-import java.util.*; 
-public class Main{
-    public static void main(String []args){
-        new Main().init();
-    }
-	public void init() {
-		/*Scanner scanner = new Scanner(System.in);
-		while(scanner.hasNext()){
-			int m = scanner.nextInt();
-			fun(m);
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long i = 0;
+		int size = 100000000;
+		while (true) {
+			i++;
+			if (i % size == 0)
+				System.out.println("test= " + i / size);
+			if (i >= Long.MAX_VALUE - 1)
+				i = 0;
+
 		}
-		scanner.close();*/
-		int [] arr = {1,2,49,34,73,44,3,4,5,6,7};
-		fun(arr);
 	}
-    public int fun(int[] data){
-    	int m=0;
-    	int max = data[0];
-    	int sec = data[0];
-    	
-    	for(int i=0;i<data.length;i++){
-    		if(data[i]>sec){
-    			//1
-    			sec = data[i];
-    		}
-    		if(sec>max){
-    			max += sec;
-    			//2
-    			sec = max-sec;
-    			max-=sec;
-    		}
-    	}
-    	//3
-    	System.out.println(max);
-    	System.out.println(sec);
-    	return max;
-    }
+
 }
