04ce437e2fee4c1fa50dee627c5d0fcd1aa5097b
xushihua <xshqhua@gmail.com>
2017-06-11 15:10:57 +0800

    add testo1

--- src/test/Test01.java
+++ src/test/Test01.java
@@ -4,48 +4,12 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-		new Test01().init();
-	}
+		new Test01().init();	}
 
 	public void init() {
 		for (int i = 0; i < 10; i++)
 			System.out.println(i);
-		for (int i = 0; i < 10; i++)
-			System.out.println(i);
-		for (int i = 0; i < 10; i++)
-			System.out.println(i);
-		for (int i = 0; i < 10; i++)
-			System.out.println(i);
-		for (int i = 0; i < 10; i++)
-			System.out.println(i);
-		for (int i = 0; i < 10; i++)
-			System.out.println(i);
-		for (int i = 0; i < 10; i++)
-			System.out.println(i);
-		for (int i = 0; i < 10; i++)
-			System.out.println(i);
-		for (int i = 0; i < 10; i++)
-			System.out.println(i);
-		for (int i = 0; i < 10; i++)
-			System.out.println(i);
-		for (int i = 0; i < 10; i++)
-			System.out.println(i);
-		for (int i = 0; i < 10; i++)
-			System.out.println(i);
+
 	}
 	
 
