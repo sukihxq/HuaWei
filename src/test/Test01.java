644d14ea57490f2fe566a6ed535fd925a99d2d26
xushihua <xshqhua@gmail.com>
2017-06-11 14:25:35 +0800

    test

--- src/test/Test01.java
+++ src/test/Test01.java
@@ -3,8 +3,20 @@ package test;
 public class Test01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		这是为了验证git
+		// TODO Auto-generated method
+		new Test01().init();
 	}
+
+	public void init() {
+		for (int i = 0; i < 10; i++)
+			System.out.println(i);
+		for (int i = 0; i < 10; i++)
+			System.out.println(i);
+		for (int i = 0; i < 10; i++)
+			System.out.println(i);
+		for (int i = 0; i < 10; i++)
+			System.out.println(i);
+	}
+	
 
 }
